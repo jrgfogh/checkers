14539138823f2b532035bf1937b30b957b6c30a9
Jørgen Fogh <jrgfogh@gmail.com>
2019-01-13 20:53:03 +0100

    Generate jumps for black men.

--- src/moveGenerator.js
+++ src/moveGenerator.js
@@ -1,6 +1,7 @@
 export const MoveKind = {
     Simple: 0,
-    Crowning: 1
+    Crowning: 1,
+    Jump: 2
 }
 
 const rowLength = 8
@@ -66,11 +67,27 @@ export default class MoveGenerator {
     movesForBlackManFrom(square) {
         const moves = [];
         const moveKind = squareIsInLastTwoRows(square) ? MoveKind.Crowning : MoveKind.Simple;
+
+        this.pushJumpsForBlackman(square, moves);
+
         if (!squareIsAtRightEdge(square))
             this.pushMoveIfNotObstructed(square + rowLength + 1, moveKind, moves);
         if (!squareIsAtLeftEdge(square))
             this.pushMoveIfNotObstructed(square + rowLength - 1, moveKind, moves);
         return moves;
+    }
+
+    pushJumpsForBlackman(square, moves) {
+        if (square < 48 &&
+                this.board[square + rowLength - 1] !== null &&
+                this.board[square + rowLength - 1].color !== "black" &&
+                this.board[square + 2 * (rowLength - 1)] === null)
+            moves.push(square + 2 * (rowLength - 1), MoveKind.Jump);
+        if (square < 46 &&
+                this.board[square + rowLength + 1] !== null &&
+                this.board[square + rowLength + 1].color !== "black" &&
+                this.board[square + 2 * (rowLength + 1)] === null)
+            moves.push(square + 2 * (rowLength + 1), MoveKind.Jump);
     }
 
     pushMoveIfNotObstructed(move, moveKind, moves) {
