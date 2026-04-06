a75a6c841f6634701f36ac9408b3f87b5d154a77
Jørgen Fogh <jrgfogh@gmail.com>
2026-04-06 15:24:23 +0200

    Update packages.
    Claude Sonnet 4.6, prompt: "Please make sure all packages are up to date, including major versions."

--- src/checkers.tsx
+++ src/checkers.tsx
@@ -1,11 +1,10 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import { Game } from './ui';
 import { parse, startPosition } from "./checkersFEN";
 
 const board = parse(startPosition).board;
 
-ReactDOM.render(
-    <Game board={ board } viewpoint="black" turn="black" />,
-    document.getElementById('game')
+createRoot(document.getElementById('game')!).render(
+    <Game board={ board } viewpoint="black" turn="black" />
   );
